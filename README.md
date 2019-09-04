dbe1a6bc3da4b4f612f2553f1847f4997f079f96
linxueyuanstdio <761516186@qq.com>
2019-09-04 13:55:07 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# TimeCat-Plugin-module-data
+# TimeCat Plugin 'module-data'
+
+## 功能
+
+数据控制中心，以表格的形式读取数据库，支持导出备份
+
+## 目录
+打包进插件包里的：
+- sample-runtime
+- sample-loader
+- sample-shadow-apk
+仅用于调试的：
+- sample-normal-apk
---
+++ plugin-main-lib/src/main/java/com/tencent/shadow/sample/plugin/DataDetailActivity.java
@@ -0,0 +1,54 @@
+package com.tencent.shadow.sample.plugin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alibaba.android.arouter.facade.annotation.Autowired;
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.bin.david.form.core.SmartTable;
+import com.timecat.component.data.RouterHub;
+import com.timecat.component.data.database.DB;
+
+/**
+ * @author dlink
+ * @email linxy59@mail2.sysu.edu.cn
+ * @date 2018/6/16
+ * @discription 权限+喵密钥
+ * @usage null
+ */
+@Route(path = RouterHub.Data_DataDetailActivity)
+public class DataDetailActivity extends AppCompatActivity {
+
+    @Autowired
+    String title = "";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        //    ARouter.getInstance().inject(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_activity_datadetail);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(title);
+
+        SmartTable table = findViewById(R.id.table);
+        table.setZoom(true);
+        if ("任务".equals(title)) {
+            table.setData(DB.schedules().findAllForActiveUser());
+        } else if ("笔记".equals(title)) {
+            table.setData(DB.notes().findAllForActiveUser());
+        } else if ("笔记本".equals(title)) {
+            table.setData(DB.notebooks().findAllForActiveUser());
+        } else if ("计划".equals(title)) {
+            table.setData(DB.plans().findAllForActiveUser());
+        } else if ("子计划".equals(title)) {
+            table.setData(DB.subPlans().findAllForActiveUser());
+        } else if ("习惯".equals(title)) {
+            table.setData(DB.habits().findAllForActiveUser());
+        }
+    }
+
+}
